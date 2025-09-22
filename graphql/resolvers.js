0a7a23b0c423519f688c417afecd6fbc6592bd78
Δhmed <BlxckBit@gmail.com>
2025-09-22 11:15:06 +0200

    GraphQL works

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -2,42 +2,52 @@ const Product = require('../models/Product');
 
 const resolvers = {
   Query: {
-    products: () => Product.find(),
-    product: (_, { id }) => Product.findById(id),
+    products: async () => await Product.find(),
+
+    product: async (_, { id }) => await Product.findById(id),
+
     totalStockValue: async () => {
       const products = await Product.find();
-      return products.reduce((sum, p) => sum + p.price * p.amountInStock, 0);
+      return products.reduce((sum, p) => sum + (p.price || 0) * (p.amountInStock || 0), 0);
     },
+
     totalStockValueByManufacturer: async () => {
       const products = await Product.find();
       const grouped = {};
       products.forEach(p => {
         const name = p.manufacturer?.name || "Unknown";
-        grouped[name] = (grouped[name] || 0) + p.price * p.amountInStock;
+        grouped[name] = (grouped[name] || 0) + (p.price || 0) * (p.amountInStock || 0);
       });
       return Object.keys(grouped).map(name => ({
         manufacturer: name,
         totalValue: grouped[name]
       }));
     },
-    lowStockProducts: () => Product.find({ amountInStock: { $lt: 10 } }),
+
+    lowStockProducts: async () => await Product.find({ amountInStock: { $lt: 151 } }),
+
     criticalStockProducts: async () => {
       const products = await Product.find({ amountInStock: { $lt: 5 } });
       return products.map(p => ({
-        manufacturerName: p.manufacturer?.name,
-        contactName: p.manufacturer?.contact?.name,
-        contactPhone: p.manufacturer?.contact?.phone,
-        contactEmail: p.manufacturer?.contact?.email,
+        manufacturerName: p.manufacturer?.name || "Unknown",
+        contactName: p.manufacturer?.contact?.name || "N/A",
+        contactPhone: p.manufacturer?.contact?.phone || "N/A",
+        contactEmail: p.manufacturer?.contact?.email || "N/A",
       }));
     },
+
     manufacturers: async () => {
       const products = await Product.find();
-      return products.map(p => p.manufacturer);
+      return products.map(p => p.manufacturer).filter(Boolean);
     }
   },
+
   Mutation: {
-    addProduct: (_, args) => Product.create(args),
-    updateProduct: (_, { id, ...rest }) => Product.findByIdAndUpdate(id, rest, { new: true }),
+    addProduct: async (_, args) => await Product.create(args),
+
+    updateProduct: async (_, { id, ...rest }) =>
+      await Product.findByIdAndUpdate(id, rest, { new: true }),
+
     deleteProduct: async (_, { id }) => {
       await Product.findByIdAndDelete(id);
       return "Deleted";
--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 
-mongoose.connect('mongodb+srv://blxckbit:Paradox11@cluster0.oyhmeov.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect('mongodb+srv://blxckbit:Paradox11@cluster0.oyhmeov.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 .then(() => {
   console.log('MongoDB connected');
   app.listen(5000, () => console.log('Server running on port 5000'));
